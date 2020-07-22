caf983e7f157344a99f3847436019b82d46732f6
Prof. Miguel Angel Medina-Pérez, PhD <47543798+miguelmedinaperez@users.noreply.github.com>
2020-07-22 15:45:50 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using PRFramework.Core.Common;
+using PRFramework.Core.IO;
+using PRFramework.Core.SupervisedClassifiers.EmergingPatterns.Classifiers;
+using System;
+
+namespace BaggingRandomMinerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var traininDataset = CsvLoader.Load(@"C:\1tra.csv", out InstanceModel trModel);
+
+            var testingDataset = CsvLoader.Load(@"C:\1tst.csv", out InstanceModel tsModel);
+
+            DTAE classifier = new DTAE();
+            classifier.Train(trModel, traininDataset);
+
+            foreach (var obj in testingDataset)
+                Console.WriteLine(classifier.Classify(obj)[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
